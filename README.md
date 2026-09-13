712f0b076ec8cf0ddc35e55610c35e2435938ef2
Burak Altunaydin <b.altunaydin@gmail.com>
2026-09-13 19:54:25 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# themodel
+# THE MODEL CASTING Catalog
+
+
+Yeni model eklemek için `models.js` dosyasındaki `models` dizisine aynı formatta yeni bir model objesi ekle
+
+Fotoğrafları klasör içine aktar ve 01.JPG olarak formatla
---
+++ models.js
@@ -0,0 +1,50 @@
+const models = [
+  {
+    id: "model-01",
+    name: "Kayra Bez",
+    cover: "images/model-01/01.jpg",
+    height: "178 cm",
+    weight: "69 kg",
+    measurements: "",
+    shoe: "43",
+    hair: "Kumral",
+    eyes: "Mavi",
+    city: "İstanbul",
+    photos: [
+      "images/model-01/01.jpg",
+      "images/model-01/02.jpg",
+      "images/model-01/03.jpg",
+      "images/model-01/04.jpg",
+      "images/model-01/05.jpg",
+      "images/model-01/06.jpg",
+      "images/model-01/07.jpg",
+      "images/model-01/08.jpg",
+      "images/model-01/09.jpg",
+      "images/model-01/10.jpg"
+    ]
+  },
+  {
+    id: "model-02",
+    name: "Yağız Toprak Sözen",
+    cover: "images/model-02/01.jpg",
+    height: "186 cm",
+    weight: "72 kg",
+    measurements: "",
+    shoe: "46",
+    hair: "Siyah",
+    eyes: "Yeşil",
+    city: "İstanbul",
+    photos: [
+      "images/model-02/01.jpg",
+      "images/model-02/02.jpg",
+      "images/model-02/03.jpg",
+      "images/model-02/04.jpg",
+      "images/model-02/05.jpg",
+      "images/model-02/06.jpg",
+      "images/model-02/07.jpg",
+      "images/model-02/08.jpg",
+      "images/model-02/09.jpg",
+      "images/model-02/10.jpg"
+    ]
+  }
+];
